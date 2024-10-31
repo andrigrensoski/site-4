dfaf76435755c7dadee106b9c5468f28598bfeb1
andrigrensoski <160618422+andrigrensoski@users.noreply.github.com>
2024-10-31 11:35:11 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,33 @@
+criaCartao(
+    'Programação',
+    'o que acontece no final do livro uma segunda chance?',
+    'Kenna Rowan retorna à cidade onde tudo deu errado após passar cinco anos na prisão'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'para qual cidade Lilly Bloom se muda após a morte de seu pai?',
+    'Boston'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+criaCartao(
+    'Lingua inglesa',
+    'personagens principais do livro novembro 9?',
+    'Ben e Fallon'
+)
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
